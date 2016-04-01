1e98a8ebda9850e5bd1cb266b0144dfdb6c63457
Denis Stepulenok <super.denis@gmail.com>
2016-04-01 12:01:29 +0300

    Занятие 8

---
+++ 08/01_Modules/module2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void myFunctionMod3();
+
+using namespace std;
+
+void myFunction() {
+  cout << "myFunction()" << endl;
+  myFunctionMod3();
+}
---
+++ 08/01_Modules/module3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void myFunctionMod3() {
+  cout << "myFunctionMod3()" << endl;
+}
